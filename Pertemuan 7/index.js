e69b62779988543bb4723d7cf77664189af06ef3
sams-ux <msamsudin423@gmail.com>
2025-05-05 10:16:37 +0700

    o

---
+++ Pertemuan 7/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const sequelize = require('./config/db');
+const mahasiswaRouter = require('./controller/mahasiswacontroller');
+
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/mahasiswa', mahasiswaRouter);
+
+// Sync database and start server
+sequelize.sync()
+    .then(() => {
+        console.log('Database connected and synchronized.');
+        const PORT = process.env.PORT || 3000;
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Unable to connect to the database:', err);
+    });
